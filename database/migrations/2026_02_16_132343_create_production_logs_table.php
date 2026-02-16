d7a6c8b956632df68afa4515adce8ba9c3b2deaa
AmriShanab <amrishanab@gmail.com>
2026-02-16 18:58:11 +0530

    Migrations Added Production, Material, Incubator

---
+++ database/migrations/2026_02_16_132343_create_production_logs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('production_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('incubator_id')->constrained();
+            $table->integer('quantity_produced');
+            $table->date('production_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('production_logs');
+    }
+};
